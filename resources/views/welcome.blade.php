201c1891f24a380af125f44487ac125f0e5e2f55
aishahisana <aishahisana119@gmail.com>
2021-11-30 22:07:21 +0530

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="vi9ewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>has</h1>
+    <a href="{{url('/')}}">home</a>
+    <a href="{{url('about')}}">about</a>
+    <a href="{{url('contact')}}">contact</a> <br>
+    <img src="{{asset('images/123.png')}}" height="100" width="100">
+    <img src="{{asset('images/Java.png')}}" height="100" width="100">
+
+
+
+
+    <h1>test h1</h1>
+    <h1>test3</h1>
+    <a href="">test</a>
+    <a href="">fdgfgj</a>
+</body>
+</html>
